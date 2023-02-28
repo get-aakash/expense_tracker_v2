829c91285782e5941b609219167864ee5dec8c9e
Aakash Acharya <mail.aakash108@gmail.com>
2023-02-28 18:36:56 +1100

    reset the input field

--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -5,7 +5,13 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 
 export const MyForm = ({addTransaction})=>{
-    const [formData, setFormData] = useState({})
+  const initialState = {
+    type:'',
+      title:'',
+      amount:'',
+      date:''
+  }
+    const [formData, setFormData] = useState(initialState)
     const handleChange = (e)=>{
         const {name, value} = e.target
         setFormData({
@@ -16,26 +22,27 @@ export const MyForm = ({addTransaction})=>{
     const handleOnSubmit = (e) =>{
         e.preventDefault()
         addTransaction(formData)
+        setFormData(initialState)
     }
-   
+   console.log(formData)
     return(
     <Form onSubmit={handleOnSubmit} className='border p-2 rounded shadow-lg'>
       <Row className='gap-1'>
         <Col md={2}>
-        <Form.Select onChange={handleChange} name='type' >
+        <Form.Select onChange={handleChange} name='type' required >
             <option value="">Type...</option>
             <option value="income">Income</option>
             <option value="expense">Expense</option>
           </Form.Select>
         </Col>
         <Col md={3}>
-          <Form.Control onChange={handleChange} required name='title' placeholder="Transaction Title" />
+          <Form.Control value={formData.title} onChange={handleChange} required name='title' placeholder="Transaction Title" />
         </Col>
         <Col md={2}>
-          <Form.Control onChange={handleChange} required name='amount' type='number' placeholder="111" />
+          <Form.Control value={formData.amount} onChange={handleChange} required name='amount' type='number' placeholder="111" />
         </Col>
         <Col md={2}>
-          <Form.Control onChange={handleChange} required type='date' name='date' />
+          <Form.Control value={formData.date} onChange={handleChange} required type='date' name='date' />
         </Col>
         <Col md={2}>
           <Button  type='submit'>
